b16bb14b963ceb739ec175ad868308ba7b8278af
jo7oem <jo7oem@gmail.com>
2023-10-15 02:37:24 +0900

    change actions

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"time"
 )
 
 func main() {
+	db, err := sql.Open("postgres", "user=hatsukari dbname=hatsukari password=hatsukari sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 	fmt.Print("Wake up!")
 	for {
 		fmt.Println("work!")
