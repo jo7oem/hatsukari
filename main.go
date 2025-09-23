2b8b8be9abbfe10882399fc2d38506db4602c6aa
jo7oem <jo7oem@gmail.com>
2025-09-23 11:15:25 +0900

    restart

--- main.go
+++ main.go
@@ -9,37 +9,6 @@ import (
 )
 
 func main() {
-	configPath := "sample/server_config.yaml"
-
-	serverConfig, err := config.LoadServerConfig(configPath)
-	if err != nil {
-		panic(err)
-	}
-
-	// handlerを構築していく
-	siteRoot, err := serverConfig.SiteRootFS()
-	if err != nil {
-		panic(err)
-	}
-
-	siteConf, err := config.LoadWebsiteConfig(siteRoot.FS(), serverConfig.SiteConfigName)
-	if err != nil {
-		panic(err)
-	}
-
-	_ = siteConf
-
-	cm, err := cache.NewCacheManager("tmp/.cache")
-	if err != nil {
-		panic(err)
-	}
-
-	defer cm.Close()
-
-	//
-
-	var r markdown.Renderer
-	_ = r
 
 	fmt.Println("End")
 }
