dcaca050d32efdd7707e76edc05f344c761976c3
jo7oem <jo7oem@gmail.com>
2025-11-03 16:59:41 +0900

    Add additional test cases for ReadMarkdownWithMeta function

--- store/markdown/markdown_test.go
+++ store/markdown/markdown_test.go
@@ -29,6 +29,59 @@ func TestReadMarkdownWithMeta(t *testing.T) {
 			wantMd:   "# これはMarkdownの内容です",
 			wantErr:  false,
 		},
+		{
+			name: "メタデータなし: 冒頭が --- ではない",
+			args: args{
+				r: io.NopCloser(strings.NewReader("# heading\n---\nこの --- は本文内")),
+			},
+			wantMeta: nil,
+			wantMd:   "# heading\n---\nこの --- は本文内",
+			wantErr:  false,
+		},
+		{
+			name: "メタデータ終端なし: 冒頭 --- から始まるが閉じない",
+			args: args{
+				r: io.NopCloser(strings.NewReader("---\ntitle: 未終端")),
+			},
+			wantMeta: nil,
+			wantMd:   "---\ntitle: 未終端",
+			wantErr:  false,
+		},
+		{
+			name:     "nil リーダー",
+			args:     args{r: nil},
+			wantMeta: nil,
+			wantMd:   "",
+			wantErr:  false,
+		},
+		{
+			name:     "空文字列",
+			args:     args{r: io.NopCloser(strings.NewReader(""))},
+			wantMeta: nil,
+			wantMd:   "",
+			wantErr:  false,
+		},
+		{
+			name:     "短いコンテンツ (<8 bytes)",
+			args:     args{r: io.NopCloser(strings.NewReader("abc"))},
+			wantMeta: nil,
+			wantMd:   "abc",
+			wantErr:  false,
+		},
+		{
+			name:     "Windows改行: メタ判定されない",
+			args:     args{r: io.NopCloser(strings.NewReader("---\r\ntitle: Win改行\r\n---\r\n# 本文"))},
+			wantMeta: nil,
+			wantMd:   "---\r\ntitle: Win改行\r\n---\r\n# 本文",
+			wantErr:  false,
+		},
+		{
+			name:     "本文中に追加の --- がある",
+			args:     args{r: io.NopCloser(strings.NewReader("---\ntitle: Second --- test\n---\n# 本文1\n---\n本文2"))},
+			wantMeta: []byte("title: Second --- test\n"),
+			wantMd:   "# 本文1\n---\n本文2",
+			wantErr:  false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -43,11 +96,11 @@ func TestReadMarkdownWithMeta(t *testing.T) {
 			}
 
 			if diff := cmp.Diff(tt.wantMeta, gotMeta); diff != "" {
-				t.Errorf("ReadMarkdownWithMeta() diff: \n%s\n", diff)
+				t.Errorf("ReadMarkdownWithMeta() diff meta:\n%s\n", diff)
 			}
 
 			if diff := cmp.Diff(tt.wantMd, gotMd); diff != "" {
-				t.Errorf("ReadMarkdownWithMeta() diff: \n%s\n", diff)
+				t.Errorf("ReadMarkdownWithMeta() diff markdown:\n%s\n", diff)
 			}
 		})
 	}
