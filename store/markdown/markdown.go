c08b4025660f8587d1d5333f4c2052bdccaf2721
jo7oem <jo7oem@gmail.com>
2025-11-03 16:57:33 +0900

    readMD

--- store/markdown/markdown.go
+++ store/markdown/markdown.go
@@ -1,17 +1,47 @@
 package markdown
 
 import (
+	"bytes"
 	"io"
 )
 
-// ParseMarkdown は、Markdown 形式のデータをパースし、HTML に変換します。
-// また、Markdown 内にメタデータが含まれている場合は、それを MdMeta 型で返します。
-// r が nil の場合、または読み取りエラーが発生した場合は、エラーを返します。
-// data メタデータを転記するデータ型。
-// data が nil の場合、メタデータの転記は行われません。
-// メタデータのフォーマットは、YAML フロントマター形式を想定しています。
-// 先頭3byteが "---" で始まり、次の "---" までがメタデータとみなされます。
-// 先頭が "---" で始まらない場合、あるいは終端する"---"が存在しない場合、メタデータは存在しないとみなされます。
-func ParseMarkdown(r io.Reader, data *any) ([]byte, error) {
-	return nil, nil
+// ReadMarkdownWithMeta は,読み込んだMarkdownファイルのメタデータと内容を返す関数です.
+// r: io.Reader - 読み込むMarkdownファイルのリーダー
+// 戻り値:
+// []byte - メタデータ
+// string - Markdown内容
+// error - エラー情報
+// メタデータはMarkdownファイルの先頭にYAML形式で記述されていることを想定.
+// 例:
+// ---
+// title: サンプルタイトル
+// date: 2024-01-01
+// ---
+// # これはMarkdownの内容です
+// `---` で囲まれた部分がメタデータとして扱われる.
+// メタデータが存在しない場合, 空のバイトスライスとMarkdown内容を返す.
+// メタデータの終端が存在しない場合、全てをMarkdown内容として返す.
+func ReadMarkdownWithMeta(r io.Reader) ([]byte, string, error) {
+	if r == nil {
+		return nil, "", nil
+	}
+
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return nil, "", err
+	}
+
+	if len(data) < 8 || string(data[0:4]) != "---\n" {
+		return nil, string(data), nil
+	}
+
+	endMetaIndex := bytes.Index(data, []byte("\n---\n"))
+	if endMetaIndex == -1 {
+		return nil, string(data), nil
+	}
+
+	metaData := data[4 : endMetaIndex+1]
+	markdownData := data[endMetaIndex+5:]
+
+	return metaData, string(markdownData), nil
 }
