ffa94a0305a72fce0ffe133052c584a1c24f78ca
jo7oem <jo7oem@gmail.com>
2026-04-05 22:17:34 +0900

    goldmarkの拡張機能を追加
    - GFM（GitHub Flavored Markdown）とCJK（Chinese, Japanese, Korean）拡張を追加しました。
    - レンダラーオプションでunsafeを有効にしました。

--- store/hosting/renderer/renderer.go
+++ store/hosting/renderer/renderer.go
@@ -11,13 +11,20 @@ import (
 	"github.com/jo7oem/hatsukari/logging"
 	"github.com/yuin/goldmark"
 	meta "github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 )
 
 var wakeUpTime = time.Now()
 var markdown = goldmark.New(
 	goldmark.WithExtensions(
 		meta.Meta,
+		extension.GFM,
+		extension.CJK,
+	),
+	goldmark.WithRendererOptions(
+		html.WithUnsafe(),
 	),
 )
 
